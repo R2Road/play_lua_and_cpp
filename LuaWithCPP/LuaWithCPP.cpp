aa1366bfd4537326ec4c61b70b60266fd226ae1a
r2road <studior2road@gmail.com>
2020-08-23 08:36:20 +0900

    1st step

--- LuaWithCPP/LuaWithCPP.cpp
+++ LuaWithCPP/LuaWithCPP.cpp
@@ -1,7 +1,50 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+extern "C"
+{
+#include "Lua54/include/lua.h"
+#include "Lua54/include/lauxlib.h"
+#include "Lua54/include/lualib.h"
+}
+
+#ifdef _WIN32
+#pragma comment( lib, "lua54/liblua54.a" )
+#endif
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	//
+	// Make Lua State
+	//
+	lua_State* lua_state_obj = luaL_newstate();
+
+	//
+	// Current Command
+	//
+	const std::string command = "a = 7 + 11";
+	std::cout << "lua command" << command.c_str() << std::endl;
+
+	//
+	// Do Command
+	//
+	const int result = luaL_dostring( lua_state_obj, command.c_str() );
+	if( result != LUA_OK )
+	{
+		const auto error_message = lua_tostring( lua_state_obj, -1 );
+		std::cout << error_message << std::endl;
+	}
+
+	//
+	// Get
+	//
+	lua_getglobal( lua_state_obj, "a" );
+	if( lua_isnumber( lua_state_obj, -1 ) )
+	{
+		const auto a = static_cast<int>( lua_tonumber( lua_state_obj, -1 ) );
+		std::cout << "result : " << "a : " << a << std::endl;
+	}
+	
+
+	return 0;
 }
