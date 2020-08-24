31147381bc155518ee97a85a31dfaac719c19565
r2road <studior2road@gmail.com>
2020-08-25 04:38:47 +0900

    update : step_DoFile

--- LuaWithCPP/step_DoFile.cpp
+++ LuaWithCPP/step_DoFile.cpp
@@ -18,18 +18,50 @@ void step_DoFile()
 	std::cout << "Call : luaL_openlibs" << std::endl;
 	luaL_openlibs( lua_state_obj );
 
-	std::cout << std::endl;	
+	std::cout << std::endl;
+
+	//
+	// Test x 0
+	//
+	{
+		std::cout << "# Test 0" << std::endl;
+
+		const int result = luaL_dofile( lua_state_obj, "Resources/step_DoFile_00.lua" );
+		if( result != LUA_OK )
+		{
+			const auto error_message = lua_tostring( lua_state_obj, -1 );
+
+			std::cout << "DoFile Failed" << std::endl;
+			std::cout << error_message << std::endl;
+		}
+		else
+		{
+			std::cout << "DoFile Success" << std::endl;
+		}
+
+		lua_getglobal( lua_state_obj, "a" );
+
+		if( lua_isnumber( lua_state_obj, -1 ) )
+		{
+			const auto a = static_cast<float>( lua_tonumber( lua_state_obj, -1 ) );
+			std::cout << "result : " << "a : " << a << std::endl;
+		}
+	}
+
+	std::cout << std::endl;
 
 	//
 	// Test x 1
 	//
 	{
+		std::cout << "# Test 1" << std::endl;
+
 		const int result = luaL_dofile( lua_state_obj, "Resources/step_DoFile_01.lua" );
 		if( result != LUA_OK )
 		{
 			const auto error_message = lua_tostring( lua_state_obj, -1 );
 
-			std::cout << "DoFile Failed " << std::endl;
+			std::cout << "DoFile Failed" << std::endl;
 			std::cout << error_message << std::endl;
 		}
 		else
@@ -52,12 +84,14 @@ void step_DoFile()
 	// Test x 2
 	//
 	{
+		std::cout << "# Test 2" << std::endl;
+
 		const int result = luaL_dofile( lua_state_obj, "Resources/step_DoFile_02.lua" );
 		if( result != LUA_OK )
 		{
 			const auto error_message = lua_tostring( lua_state_obj, -1 );
 
-			std::cout << "DoFile Failed " << std::endl;
+			std::cout << "DoFile Failed" << std::endl;
 			std::cout << error_message << std::endl;
 		}
 		else
