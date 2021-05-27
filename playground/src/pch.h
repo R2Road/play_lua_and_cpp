01a179bbecdf31cd1d572b4ddd23d9cdbb903aad
r2road <studior2road@gmail.com>
2021-05-28 07:44:07 +0900

    fix

--- playground/src/pch.h
+++ playground/src/pch.h
@@ -5,9 +5,9 @@
 
 extern "C"
 {
-#include "Lua54/include/lua.h"
-#include "Lua54/include/lauxlib.h"
-#include "Lua54/include/lualib.h"
+#include "../Lua54/include/lua.h"
+#include "../Lua54/include/lauxlib.h"
+#include "../Lua54/include/lualib.h"
 }
 
 #endif //PCH_H
