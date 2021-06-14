5285679dac556f2371ccd998df3d5b01a53edbea
r2road <studior2road@gmail.com>
2021-06-14 19:35:08 +0900

    update : lua_getglobal_test - Basic

--- playground/src/test/step_GetGlobal.cpp
+++ playground/src/test/step_GetGlobal.cpp
@@ -24,49 +24,54 @@ namespace lua_getglobal_test
 			lua_State* lua_state_obj = luaL_newstate();
 
 			{
-				std::cout << "lua_getglobal a" << r2::linefeed;
-
 				//
 				// Push Variable "a"
 				//
 				const int type = lua_getglobal( lua_state_obj, "a" );
+
+				std::cout << r2::tab << "lua_getglobal( lua_state_obj, \"a\" );" << r2::linefeed;
+				std::cout << r2::tab2 << "Result : ";
 				step_helper::PrintType( "a", type );
 			}
 
-			std::cout << r2::linefeed;
+			std::cout << r2::split;
 
 			{
-				const std::string command = "b = 7";
-				std::cout << "Command : " << command.c_str() << r2::linefeed;
+				const std::string command = "a = 7";
+				std::cout << r2::tab << "+ Command String" << r2::linefeed2;
+				std::cout << r2::tab2 << "const std::string command = \"a = 7\";" << r2::linefeed2;
 
-				//
-				// Do String
-				//
+
+				std::cout << r2::tab << "+ Process" << r2::linefeed2;
+				std::cout << r2::tab2 << "luaL_dostring( lua_state_obj, command.c_str() )" << r2::linefeed;
+
 				const int result = luaL_dostring( lua_state_obj, command.c_str() );
 				if( result != LUA_OK )
 				{
 					const auto error_message = lua_tostring( lua_state_obj, -1 );
 
-					std::cout << "Command Failed " << r2::linefeed;
-					std::cout << error_message << r2::linefeed;
+					std::cout << r2::tab3 << "Command Failed " << r2::linefeed;
+					std::cout << r2::tab3 << error_message << r2::linefeed;
 				}
 				else
 				{
-					std::cout << "Command Success" << r2::linefeed;
+					std::cout << r2::tab3 << "Command Success" << r2::linefeed;
 				}
-
-				std::cout << "lua_getglobal b" << r2::linefeed;
-
-				//
-				// Push Variable "a"
-				//
-				const int type = lua_getglobal( lua_state_obj, "b" );
-				step_helper::PrintType( "b", type );
 			}
 
-			lua_close( lua_state_obj );
+			std::cout << r2::split;
+				
+			{
+				const int type = lua_getglobal( lua_state_obj, "a" );
+
+				std::cout << r2::tab << "lua_getglobal( lua_state_obj, \"a\" );" << r2::linefeed;
+				std::cout << r2::tab2 << "Result : ";
+				step_helper::PrintType( "a", type );
+			}
 
 			std::cout << r2::split;
+
+			lua_close( lua_state_obj );
 
 			return r2::eTestResult::RunTest;
 		};
