084310a70269d984cd8c499a2790924822fab880
r2road <studior2road@gmail.com>
2022-03-26 11:40:55 +0900

    ...

--- playground/src/test_lua/lua_dostring_test.cpp
+++ playground/src/test_lua/lua_dostring_test.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "lua_dostring_test.h"
 
+#include "r2/r2_Inspector.h"
 #include "r2cm/r2cm_eTestEndAction.h"
 
 namespace lua_dostring_test
@@ -18,31 +19,45 @@ namespace lua_dostring_test
 		{
 			std::cout << "# " << GetInstance().GetTitleFunction()( ) << " #" << r2::linefeed;
 
+			std::cout << r2::split;
 
-			lua_State* lua_state_obj = luaL_newstate();
-
+			DECLARATION_SUB( lua_State* lua_state_obj = luaL_newstate() );
 
 			std::cout << r2::split;
 
 			{
-				const char* command = "a = 7 + 11";
-				std::cout << r2::tab << "+ Command String" << r2::linefeed2;
-				std::cout << r2::tab2 << "const char* command = \"a = 7 + 11\";" << r2::linefeed;
+				DECLARATION_MAIN( const char* command = "a = 7 + 11" );
 
+				std::cout << r2::linefeed;
 
-				std::cout << r2::linefeed2;
-
-
-				std::cout << r2::tab << "+ Process" << r2::linefeed2;
-				std::cout << r2::tab2 << "luaL_dostring( lua_state_obj, command )" << r2::linefeed;
-
-				const int result = luaL_dostring( lua_state_obj, command );
+				DECLARATION_MAIN( const int result = luaL_dostring( lua_state_obj, command ) );
 				if( result != LUA_OK )
 				{
 					const auto error_message = lua_tostring( lua_state_obj, -1 );
 
-					std::cout << r2::tab3 << "Command Failed " << r2::linefeed;
-					std::cout << r2::tab3 << error_message << r2::linefeed;
+					std::cout << r2::tab << "Command Failed " << r2::linefeed;
+					std::cout << r2::tab << error_message << r2::linefeed;
+				}
+				else
+				{
+					std::cout << r2::tab << "Command Success" << r2::linefeed;
+				}
+			}
+
+			std::cout << r2::split;
+
+			{
+				DECLARATION_MAIN( const char* command = "a = 7 + " );
+
+				std::cout << r2::linefeed;
+
+				DECLARATION_MAIN( const int result = luaL_dostring( lua_state_obj, command ) );
+				if( result != LUA_OK )
+				{
+					const auto error_message = lua_tostring( lua_state_obj, -1 );
+
+					std::cout << r2::tab << "Command Failed " << r2::linefeed;
+					std::cout << r2::tab << error_message << r2::linefeed;
 				}
 				else
 				{
@@ -52,37 +67,9 @@ namespace lua_dostring_test
 
 			std::cout << r2::split;
 
-			{
-				const char* command = "a = 7 + ";
-				std::cout << r2::tab << "+ Command String" << r2::linefeed2;
-				std::cout << r2::tab2 << "const char* command = \"a = 7 + \";" << r2::linefeed;
-
-
-				std::cout << r2::linefeed2;
-
-
-				std::cout << r2::tab << "+ Process" << r2::linefeed2;
-				std::cout << r2::tab2 << "luaL_dostring( lua_state_obj, command )" << r2::linefeed;
-
-				const int result = luaL_dostring( lua_state_obj, command );
-				if( result != LUA_OK )
-				{
-					const auto error_message = lua_tostring( lua_state_obj, -1 );
-
-					std::cout << r2::tab3 << "Command Failed " << r2::linefeed;
-					std::cout << r2::tab3 << error_message << r2::linefeed;
-				}
-				else
-				{
-					std::cout << r2::tab3 << "Command Success" << r2::linefeed;
-				}
-			}
+			PROCESS_SUB( lua_close( lua_state_obj ) );
 
 			std::cout << r2::split;
-
-
-			lua_close( lua_state_obj );
-
 
 			return r2cm::eTestEndAction::Pause;
 		};
