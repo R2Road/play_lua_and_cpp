19a0c5453f6bd127b7f1db2b0df67dbbd4d7ddd5
r2road <studior2road@gmail.com>
2022-04-07 01:11:28 +0900

    update : test_lua - lua_table_test - GetValueTest

--- playground/src/test_lua/lua_table_test.cpp
+++ playground/src/test_lua/lua_table_test.cpp
@@ -114,13 +114,11 @@ namespace lua_table_test
 			std::cout << r2::split;
 
 			DECLARATION_SUB( lua_State* lua_state_obj = luaL_newstate() );
-			DECLARATION_SUB( luaL_openlibs( lua_state_obj ) );
-			PROCESS_SUB( test_lua_helper::DoFile_Silent( lua_state_obj, "resources/step_GetTable_01.lua" ) );
 
 			std::cout << r2::split;
 
 			{
-				PROCESS_MAIN( lua_getglobal( lua_state_obj, "data" ) );
+				PROCESS_MAIN( lua_newtable( lua_state_obj ) );
 				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
 			}
 
@@ -129,28 +127,37 @@ namespace lua_table_test
 			{
 				std::cout << r2::tab << "+ Ready" << r2::linefeed2;
 
-				PROCESS_MAIN( lua_pushstring( lua_state_obj, "name" ) );
-				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
-
-				std::cout << r2::linefeed;
-				std::cout << r2::tab << "+ Get" << r2::linefeed2;
-
-				PROCESS_MAIN( lua_gettable( lua_state_obj, 1 ) );
-				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
+				PROCESS_MAIN( lua_pushstring( lua_state_obj, "dummy_int" ) );
+				PROCESS_MAIN( lua_pushnumber( lua_state_obj, 777 ) );
 
 				std::cout << r2::linefeed;
 
-				PROCESS_MAIN( lua_pop( lua_state_obj, 1 ) );
+				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
 			}
 
 			std::cout << r2::split;
 
 			{
-				lua_pushstring( lua_state_obj, "age" );
-				lua_gettable( lua_state_obj, -2 );
-				const int age = (int)lua_tointeger( lua_state_obj, -1 );
-				std::cout << "age : " << age << r2::linefeed;
-				lua_pop( lua_state_obj, 1 );
+				std::cout << r2::tab << "+ Push" << r2::linefeed2;
+
+				PROCESS_MAIN( lua_settable( lua_state_obj, 1 ) );
+
+				std::cout << r2::linefeed;
+
+				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "+ Get" << r2::linefeed2;
+
+				PROCESS_MAIN( lua_pushstring( lua_state_obj, "dummy_int" ) );
+				PROCESS_MAIN( lua_gettable( lua_state_obj, 1 ) );
+
+				std::cout << r2::linefeed;
+
+				PROCESS_MAIN( test_lua_helper::PrintAllStack( lua_state_obj ) );
 			}
 
 			std::cout << r2::split;
