18d431bd1d0bf61c0b1a63736dbd250cb213f6c3
r2road <studior2road@gmail.com>
2022-04-07 13:03:40 +0900

    update : test_lua - stack_test - GetTest

--- playground/src/test_lua/stack_test.cpp
+++ playground/src/test_lua/stack_test.cpp
@@ -88,46 +88,45 @@ namespace stack_test
 			{
 				std::cout << r2::tab << "+ Push( info : lua.h 233 line )" << r2::linefeed2;
 
-				PROCESS_MAIN( lua_pushnil( lua_state_obj ) );
-				PROCESS_MAIN( lua_pushnumber( lua_state_obj, 12345.12345 ) );
-				PROCESS_MAIN( lua_pushinteger( lua_state_obj, 7 ) );
-				PROCESS_MAIN( lua_pushlstring( lua_state_obj, "dummy_text", 3 ) );
 				PROCESS_MAIN( lua_pushstring( lua_state_obj, "dummy_text" ) );
-				PROCESS_MAIN( lua_pushcclosure( lua_state_obj, &cclosure_test_function, 0 ) );
-				PROCESS_MAIN( lua_pushboolean( lua_state_obj, true ) );
+
+				std::cout << r2::linefeed;
+
+				DECLARATION_MAIN( const auto stack_count = lua_gettop( lua_state_obj ) );
+				std::cout << r2::tab << "stack_count : " << stack_count << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				DECLARATION_MAIN( const auto stack_count = lua_gettop( lua_state_obj ) );
+				std::cout << r2::tab << "+ Get" << r2::linefeed2;
 
-				std::cout << r2::linefeed;
+				DECLARATION_MAIN( const auto type_name = luaL_typename( lua_state_obj, 1 ) );
+				std::cout << r2::tab << "type_name : " << type_name << r2::linefeed2;
 
-				std::cout << r2::tab << "Stack Count : " << stack_count << r2::linefeed;
-				for( int i = 1, ri = -stack_count; stack_count >= i; ++i, ++ri )
-				{
-					std::cout << r2::tab << "[" << i << "]" << r2::tab << "[" << ri << "]" << r2::tab << luaL_typename( lua_state_obj, i ) << r2::tab;
+				DECLARATION_MAIN( const auto value = lua_tostring( lua_state_obj, 1 ) );
+				std::cout << r2::tab << "value : " << value << r2::linefeed;
+			}
 
-					switch( lua_type( lua_state_obj, i ) )
-					{
-					case LUA_TNUMBER:
-						std::cout << lua_tonumber( lua_state_obj, i ) << r2::linefeed;
-						break;
-					case LUA_TSTRING:
-						std::cout << lua_tostring( lua_state_obj, i ) << r2::linefeed;
-						break;
-					case LUA_TBOOLEAN:
-						std::cout << ( lua_toboolean( lua_state_obj, i ) ? "true" : "false" ) << r2::linefeed;
-						break;
-					case LUA_TNIL:
-						std::cout << "nil" << r2::linefeed;
-						break;
-					default:
-						std::cout << lua_topointer( lua_state_obj, i ) << r2::linefeed;
-						break;
-					}
-				}
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "+ Casting?" << r2::linefeed2;
+
+				DECLARATION_MAIN( const auto value = lua_tonumber( lua_state_obj, 1 ) );
+				std::cout << r2::tab << "value : " << value << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "+ 0?" << r2::linefeed2;
+
+				DECLARATION_MAIN( const auto type_name = luaL_typename( lua_state_obj, 0 ) );
+				std::cout << r2::tab << "type_name : " << type_name << r2::linefeed2;
+
+				DECLARATION_MAIN( const auto value = lua_tonumber( lua_state_obj, 0 ) );
+				std::cout << r2::tab << "value : " << value << r2::linefeed;
 			}
 
 			std::cout << r2::split;
