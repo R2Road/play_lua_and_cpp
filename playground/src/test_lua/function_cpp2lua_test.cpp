2f113696318bf32995911200876fd81942ad8673
r2road <studior2road@gmail.com>
2022-04-12 02:12:51 +0900

    update : test_lua - function_cpp2lua_test - Argument2

--- playground/src/test_lua/function_cpp2lua_test.cpp
+++ playground/src/test_lua/function_cpp2lua_test.cpp
@@ -88,30 +88,31 @@ namespace function_cpp2lua_test
 			std::cout << r2::split;
 
 			std::cout << r2::tab << "+ Add Function : Sum( arg1, arg2 )" << r2::linefeed2;
-			PROCESS_MAIN( test_lua_helper::DoFile_Silent( lua_state_obj, "resources/function_cpp2lua_test_01.lua" ) );
 			DECLARATION_MAIN( const int arg_count = 2 );
+			PROCESS_MAIN( test_lua_helper::DoFile_Silent( lua_state_obj, "resources/function_cpp2lua_test_01.lua" ) );
 
 			std::cout << r2::split;
 
 			{
-				std::cout << r2::tab << "+ Ready With Arg x 1" << r2::linefeed2;
+				std::cout << r2::tab << "+ Ready : 인자 1개 부족" << r2::linefeed2;
 
 				PROCESS_MAIN( lua_getglobal( lua_state_obj, "Sum" ) );
 				PROCESS_MAIN( lua_pushnumber( lua_state_obj, 123 ) );
 				test_lua_helper::PrintAllStack( lua_state_obj );
-
-				std::cout << r2::linefeed;
-
-				EXPECT_TRUE( lua_isfunction( lua_state_obj, 1 ) );
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << r2::tab << "+ Call With Arg x 1" << r2::linefeed2;
+				std::cout << r2::tab << "+ Call : 인자 1개 부족" << r2::linefeed2;
 
 				EXPECT_EQ( LUA_ERRRUN, lua_pcall( lua_state_obj, arg_count, LUA_MULTRET, 0 ) );
+
+				std::cout << r2::linefeed;
+
 				test_lua_helper::PrintLuaError( lua_state_obj );
+
+				std::cout << r2::linefeed;
 
 				test_lua_helper::PrintAllStack( lua_state_obj );
 			}
@@ -119,7 +120,7 @@ namespace function_cpp2lua_test
 			std::cout << r2::split;
 
 			{
-				std::cout << r2::tab << "+ Ready With Arg x 2" << r2::linefeed2;
+				std::cout << r2::tab << "+ Ready" << r2::linefeed2;
 
 				PROCESS_MAIN( lua_getglobal( lua_state_obj, "Sum" ) );
 				PROCESS_MAIN( lua_pushnumber( lua_state_obj, 123 ) );
@@ -130,14 +131,13 @@ namespace function_cpp2lua_test
 			std::cout << r2::split;
 
 			{
-				std::cout << r2::tab << "+ Call With Arg x 2" << r2::linefeed2;
+				std::cout << r2::tab << "+ Call" << r2::linefeed2;
 
 				EXPECT_EQ( LUA_OK, lua_pcall( lua_state_obj, arg_count, LUA_MULTRET, 0 ) );
 
 				std::cout << r2::linefeed;
 
-				DECLARATION_MAIN( const auto result = lua_tonumber( lua_state_obj, -1 ) );
-				std::cout << r2::tab << "result : " << result << r2::linefeed;
+				test_lua_helper::PrintAllStack( lua_state_obj );
 			}
 
 			std::cout << r2::split;
