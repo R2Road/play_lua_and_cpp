c5344d9967e6cd361e30c85f07625d1216630f6e
R2Road <studior2road@gmail.com>
2022-11-12 05:18:32 +0900

    update r2cm x 3

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -4,14 +4,13 @@
 // - http://lua-users.org/wiki/MathLibraryTutorial
 //
 
-#include "pch.h"
-#include <Windows.h>
-
 //#if defined( DEBUG ) || defined( _DEBUG )
 //	#include <vld.h>
 //#endif
 
 #include "r2cm/r2cm_Director.h"
+#include "r2cm/r2cm_WindowUtility.h"
+
 #include "MainMenu.h"
 
 int main()
@@ -19,22 +18,17 @@ int main()
 	//
 	// Environment : Title
 	//
-	SetConsoleTitle( TEXT( "lua_and_cpp_playground" ) );
+	r2cm::WindowUtility::ChangeTitle( "lua_and_cpp_playground" );
 
 	//
 	// Environment : Size
 	//
-	system( "mode con lines=60 cols=120" );
+	r2cm::WindowUtility::Resize( 960, 960 );
 
 	//
 	// Environment : Position
 	//
-	{
-		HWND hWnd = GetConsoleWindow();
-		RECT rectClient;
-		GetClientRect( hWnd, &rectClient );
-		MoveWindow( hWnd, 0, 0, rectClient.right - rectClient.left, rectClient.bottom - rectClient.top, TRUE );
-	}
+	r2cm::WindowUtility::Move( 0, 0 );
 
 	//
 	// Setup
