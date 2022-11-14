c30d972ec5037429527e753b409de79235f9a71e
R2Road <studior2road@gmail.com>
2022-11-14 20:39:08 +0900

    cleanup

--- playground/src/test_lua/TestLuaRootMenu.cpp
+++ playground/src/test_lua/TestLuaRootMenu.cpp
@@ -7,7 +7,6 @@
 #include "test/luaL_dofile_test.h"
 #include "test/luaL_dostring_test.h"
 #include "test/luaL_openlibs_test.h"
-#include "test/metatable_test.h"
 
 #include "FunctionMenu.h"
 #include "GlobalMenu.h"
@@ -27,50 +26,20 @@ r2cm::MenuUp TestLuaRootMenu::Create( r2cm::Director& director )
 
 	{
 		ret->AddItem( '1', lua_state_test::Basic::GetInstance() );
-		ret->AddItem(
-			'2'
-			, []()->const char* { return StackMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( StackMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'3'
-			, []()->const char* { return GlobalMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( GlobalMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<StackMenu>( '2' );
+		ret->AddMenu<GlobalMenu>( '3' );
 		ret->AddItem( '4', luaL_dostring_test::Basic::GetInstance() );
 		ret->AddItem( '5', luaL_dofile_test::Basic::GetInstance() );
 		ret->AddItem( '6', luaL_openlibs_test::Basic::GetInstance() );
 
 
+
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'q'
-			, []()->const char* { return FunctionMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( FunctionMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'w'
-			, []()->const char* { return TableMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( TableMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+
+		ret->AddMenu<FunctionMenu>( 'q' );
+		ret->AddMenu<TableMenu>( 'w' );
 
 
 
@@ -87,15 +56,7 @@ r2cm::MenuUp TestLuaRootMenu::Create( r2cm::Director& director )
 
 
 
-		ret->AddItem(
-			27
-			, []()->const char* { return MainMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( MainMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<MainMenu>( 27 );
 	}
 
 	return ret;
