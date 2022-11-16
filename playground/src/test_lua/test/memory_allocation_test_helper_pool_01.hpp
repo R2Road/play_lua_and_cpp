86bd7939e32596ead2dc70a426870567ea4dfc95
R2Road <studior2road@gmail.com>
2022-11-16 14:10:29 +0900

    update test : test_lua - memory_allocation_test - Pool_1

--- playground/src/test_lua/test/memory_allocation_test_helper_pool_01.hpp
+++ playground/src/test_lua/test/memory_allocation_test_helper_pool_01.hpp
@@ -2,52 +2,16 @@
 
 struct LuaMemoryPool_01
 {
-	void* mBegin;
-	void* mEnd;
-	void* mCurrent;
-	size_t mAllocatedSize;
-
-	LuaMemoryPool_01( void* begin, void* end ) :
-		mBegin( begin )
-		, mEnd( end )
-		, mCurrent( begin )
-	{}
-	~LuaMemoryPool_01()
-	{
-		OutputInfo();
-	}
-
-	void OutputInfo()
-	{
-		std::cout << "+ Memory Pool Info\n";
-		std::cout << "\t> Beg    : " << uint64_t( mBegin ) << "\n";
-		std::cout << "\t> End    : " << uint64_t( mEnd ) << "\n";
-		std::cout << "\t> Cur    : " << uint64_t( mCurrent ) << "\n";
-		std::cout << "\t> Size   : " << uint64_t( mEnd ) - uint64_t( mBegin ) + 1 << "\n";
-		std::cout << "\t> Use    : " << uint64_t( mCurrent ) - uint64_t( mBegin ) << "\n";
-		std::cout << "\t> Remain : " << uint64_t( mEnd ) - uint64_t( mCurrent ) + 1 << "\n";
-	}
-
-	void* Allocate( size_t size )
-	{
-		std::cout << "Allocate : " << size << "\n";
-
-		void* ptr = mCurrent;
-		mCurrent = (char*)mCurrent  + size;
-		return ptr;
-	}
-
 	void Deallocate( void* p, size_t size )
 	{
 		std::cout << "Deallocate : " << size  << "\n";
+		free( p );
 	}
 
-	void* Realloc( void* p, size_t osize, size_t nsize )
+	void* Reallocate( void* p, size_t osize, size_t nsize )
 	{
-		void* ret = Allocate( nsize );
-		memcpy( ret, p, osize );
-
-		return ret;
+		std::cout << "Realloc : " << nsize << "\n";
+		return realloc( p, nsize );
 	}
 
 	static void *l_alloc( void *ud, void *ptr, size_t osize, size_t nsize ) {
@@ -57,19 +21,10 @@ struct LuaMemoryPool_01
 		
 		if( nsize == 0 )
 		{
-			if( nullptr != ptr )
-			{
-				pool->Deallocate( ptr, osize );
-			}
-
+			pool->Deallocate( ptr, osize );
 			return NULL;
 		}
 
-		if( ptr == nullptr )
-		{
-			return pool->Allocate( nsize );
-		}
-
-		return pool->Realloc( ptr, osize, nsize );
+		return pool->Reallocate( ptr, osize, nsize );
 	}
 };
